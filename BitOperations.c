4eb5a9a3535f9793466fd518a80b0eff039c2840
JackH97 <g00359747@gmit.ie>
2021-02-08 18:00:20 +0000

    Bit masks

--- BitOperations.c
+++ BitOperations.c
@@ -6,7 +6,7 @@ void bin_print(int i) {
     // Temporary Variable.
     int k;
 
-    // Loop over the number of bits in i.
+    // Loop over the number of bits in i, left to right.
     for (j-- ; j >=0; j--) {
         //
         k = ((1 << j) & i) ? 1 : 0;  // Tuniery operator
@@ -20,23 +20,26 @@ void bin_print(int i) {
 int main(int argc, char *argv[]) {
 
     // Set i to a literal value.
-    unsigned int i = 0xffffffff; //1; //0xf1; //241 //4294967295
+    unsigned int i = 0x0f0f0f0f; //1; //0xf1; //241 //4294967295
 
     // What we're printing.
-    printf("Original:  ");
+    printf("Original:\t");
     // Print i in binary.
     bin_print(i);
     // End line.
-    printf("\t%x\t%d\n", i, i);
+    printf("\t%x\t%u\n\n", i, i);
     
+    // 32
+    int j = sizeof(unsigned int) * 8;
 
-    for (int j = 0; j < 40; j++) {
-        // What the operation is. 
-        printf("%3u << %2d: ", i, j);
-        // i shifted left j times
-        bin_print(i << j);
-        // End line.
-        printf("\n");
+    for (j--; j >= 0; j--) {
+        // 1 shifted left j times.
+        bin_print(1 << j); printf("\n");
+        // i.
+        bin_print(i); printf("\n");
+        printf("-------------------------------- &\n");
+        // (1 shifted left j times) bitwise logical and i.
+        bin_print((1 << j) & i); printf("\n\n");
     }
 
     // Everything ok
