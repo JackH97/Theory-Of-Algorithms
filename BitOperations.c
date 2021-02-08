91f22e0b2705301e8d70e2a847bf39a973b4e0c6
JackH97 <g00359747@gmit.ie>
2021-02-08 16:52:54 +0000

    Review Bit Operations Video done - Week 3

--- BitOperations.c
+++ BitOperations.c
@@ -6,27 +6,39 @@ void bin_print(int i) {
     // Temporary Variable.
     int k;
 
+    // Loop over the number of bits in i.
     for (j-- ; j >=0; j--) {
+        //
         k = ((1 << j) & i) ? 1 : 0;  // Tuniery operator
+        // Print k.
         printf("%d", k);
         
     }
 }
 
+// Standard main signature.
 int main(int argc, char *argv[]) {
 
-    int i = 241; 
+    // Set i to a literal value.
+    unsigned int i = 0xffffffff; //1; //0xf1; //241 //4294967295
 
+    // What we're printing.
     printf("Original:  ");
+    // Print i in binary.
     bin_print(i);
-    printf("\n");
-    printf("\n");
+    // End line.
+    printf("\t%x\t%d\n", i, i);
+    
 
     for (int j = 0; j < 40; j++) {
-        printf("%3d << %2d: ", i, j);
+        // What the operation is. 
+        printf("%3u << %2d: ", i, j);
+        // i shifted left j times
         bin_print(i << j);
+        // End line.
         printf("\n");
     }
 
+    // Everything ok
     return 0;
 }
