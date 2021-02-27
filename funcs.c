1c11ec49791e072da8e64f6a90c5787c5f766d05
JackH97 <g00359747@gmit.ie>
2021-02-27 19:06:41 +0000

    Added the uint32_t function in the main method for x,y,z

--- funcs.c
+++ funcs.c
@@ -1,13 +1,22 @@
 #include <stdio.h>
+#include <inttypes.h>
 
-int Ch(int x, int y, int z)
+#define WORD uint32_t
+
+uint32_t ch(uint32_t x, uint32_t y, uint32_t z)
 {
     return (x&y)^(~x&z);
 }
 
 int main(int argc, char *argv[])
 {
-    
+    uint32_t x = 0xf0f0f0f0;
+    uint32_t y = 0x0f0f0f0f;
+    uint32_t z = 0xf0f0f0f0;
+
+    uint32_t ans = Ch(x, y, z);
+
+    print("Ch(%d,%d,%d)=%d\n", x, y, z, ans);
 
     return 0;
 }
