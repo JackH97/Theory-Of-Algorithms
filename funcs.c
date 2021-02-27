e0d75e15c3a699ee19784c58165cbcfd2ecf1876
JackH97 <g00359747@gmit.ie>
2021-02-27 20:18:15 +0000

    Added in WORD instead uint32_t and PRIX32 in Ch method and main method

--- funcs.c
+++ funcs.c
@@ -2,21 +2,23 @@
 #include <inttypes.h>
 
 #define WORD uint32_t
+#define PF PRIX32
 
-uint32_t Ch(uint32_t x, uint32_t y, uint32_t z)
+WORD Ch(WORD x, WORD y, WORD z)
 {
     return (x&y)^(~x&z);
 }
 
+
 int main(int argc, char *argv[])
 {
-    uint32_t x = 0xf0f0f0f0;
-    uint32_t y = 0x0f0f0f0f;
-    uint32_t z = 0xf0f0f0f0;
+    WORD x = 0x0f0f0f0f;
+    WORD y = 0x0f0f0f0f;
+    WORD z = 0xf0f0f0f0;
 
-    uint32_t ans = Ch(x, y, z);
+    WORD ans = Ch(x, y, z);
 
-    printf("Ch(%08" PRIX32 ",%08" PRIX32 ",%08" PRIX32 ")=%08" PRIX32 "\n", x, y, z, ans);
+    printf("Ch(%08" PF ",%08" PF ",%08" PF ")=%08" PF "\n", x, y, z, ans);
 
     return 0;
 }
