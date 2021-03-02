789bf1b20ff7d9c760afbca1f598439f40d1dc92
JackH97 <g00359747@gmit.ie>
2021-03-02 19:41:41 +0000

    Added in the SIG0 function

--- funcs.c
+++ funcs.c
@@ -5,17 +5,19 @@
 #define WORD uint32_t
 #define PF PRIX32
 
-// Page 10 of the secure hash standard
-#define CH(x,y,z) (x&y)^(~x&z)
-#define MAJ(x,y,z) (x&y)^( x&z)^(y&z)
-
 // Page 5 of the secure hash standard
 #define ROTL(x,n) (x<<n)|(x>>(W-n))
 #define ROTR(x,n) (x>>n)|(x<<(W-n))
 
+// Page 10 of the secure hash standard
+#define CH(x,y,z) (x&y)^(~x&z)
+#define MAJ(x,y,z) (x&y)^(x&z)^(y&z)
+#define SIG0(x) ROTR(x,2)^ROTR(x,13)^ROTR(x,22)
+
+
 int main(int argc, char *argv[])
 {
-    WORD x = 0x0F0F0F0F;
+    WORD x = 0xF1234567;
     WORD y = 0x0A0A0A0A;
     WORD z = 0xB0B0B0B0;
 
@@ -30,6 +32,7 @@ int main(int argc, char *argv[])
 
     printf("ROTL(%08" PF " -> %08" PF "\n", x, ROTL(x,4));
     printf("ROTR(%08" PF " -> %08" PF "\n", x, ROTR(x,4));
+    printf("SIG0(%08" PF " -> %08" PF "\n", x, SIG0(x));
 
     return 0;
 }
