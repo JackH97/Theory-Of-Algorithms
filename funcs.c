5da3c254f63344bf9b7e42417d59536caf3ae043
JackH97 <g00359747@gmit.ie>
2021-03-02 19:26:56 +0000

    Added the ROTL & ROTR methods

--- funcs.c
+++ funcs.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <inttypes.h>
 
+#define W 32
 #define WORD uint32_t
 #define PF PRIX32
 
@@ -8,6 +9,9 @@
 #define CH(x,y,z) (x&y)^(~x&z)
 #define MAJ(x,y,z) (x&y)^( x&z)^(y&z)
 
+// Page 5 of the secure hash standard
+#define ROTL(x,n) (x<<n)|(x>>(W-n))
+#define ROTR(x,n) (x>>n)|(x<<(W-n))
 
 int main(int argc, char *argv[])
 {
@@ -22,6 +26,10 @@ int main(int argc, char *argv[])
 
     ans = MAJ(x,y,z);
     printf("Maj(%08" PF ",%08" PF ",%08" PF ")=%08" PF "\n", x, y, z, ans);
+
+
+    printf("ROTL(%08" PF " -> %08" PF "\n", x, ROTL(x,4));
+    printf("ROTR(%08" PF " -> %08" PF "\n", x, ROTR(x,4));
 
     return 0;
 }
