80e381635236b182a4268bfd9c9e50baaf15e256
JackH97 <g00359747@gmit.ie>
2021-03-02 19:52:14 +0000

    Added in SIG1,Sig0 & Sig1 into the c file

--- funcs.c
+++ funcs.c
@@ -8,11 +8,16 @@
 // Page 5 of the secure hash standard
 #define ROTL(x,n) (x<<n)|(x>>(W-n))
 #define ROTR(x,n) (x>>n)|(x<<(W-n))
+#define SHR(x,n) x>>n
 
 // Page 10 of the secure hash standard
 #define CH(x,y,z) (x&y)^(~x&z)
 #define MAJ(x,y,z) (x&y)^(x&z)^(y&z)
+
 #define SIG0(x) ROTR(x,2)^ROTR(x,13)^ROTR(x,22)
+#define SIG1(x) ROTR(x,6)^ROTR(x,11)^ROTR(x,25)
+#define Sig0(x) ROTR(x,7)^ROTR(x,18)^SHR(x,3)
+#define Sig1(x) ROTR(x,17)^ROTR(x,19)^SHR(x,10)
 
 
 int main(int argc, char *argv[])
@@ -32,7 +37,11 @@ int main(int argc, char *argv[])
 
     printf("ROTL(%08" PF " -> %08" PF "\n", x, ROTL(x,4));
     printf("ROTR(%08" PF " -> %08" PF "\n", x, ROTR(x,4));
+    printf("SHR(%08" PF " -> %08" PF "\n", x, SHR(x,4));
     printf("SIG0(%08" PF " -> %08" PF "\n", x, SIG0(x));
+    printf("SIG1(%08" PF " -> %08" PF "\n", x, SIG1(x));
+    printf("Sig0(%08" PF " -> %08" PF "\n", x, Sig0(x));
+    printf("Sig1(%08" PF " -> %08" PF "\n", x, Sig1(x));
 
     return 0;
 }
