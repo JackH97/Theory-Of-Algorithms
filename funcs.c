84558491b7262603c7b89578580f3c66afe36cbd
JackH97 <g00359747@gmit.ie>
2021-03-02 19:00:35 +0000

    Added the #define CH at beginning of c file and changed values in the word variables in main method

--- funcs.c
+++ funcs.c
@@ -3,6 +3,7 @@
 
 #define WORD uint32_t
 #define PF PRIX32
+#define CH(x,y,z) (x&y)^(~x&z)
 
 WORD Ch(WORD x, WORD y, WORD z)
 {
@@ -12,11 +13,11 @@ WORD Ch(WORD x, WORD y, WORD z)
 
 int main(int argc, char *argv[])
 {
-    WORD x = 0x0f0f0f0f;
-    WORD y = 0x0f0f0f0f;
-    WORD z = 0xf0f0f0f0;
+    WORD x = 0x0F0F0F0F;
+    WORD y = 0x0A0A0A0A;
+    WORD z = 0xB0B0B0B0;
 
-    WORD ans = Ch(x, y, z);
+    WORD ans = CH(x, y, z);
 
     printf("Ch(%08" PF ",%08" PF ",%08" PF ")=%08" PF "\n", x, y, z, ans);
 
