a3651ab8c608617853c82529f211f267d1c7b7d0
JackH97 <g00359747@gmit.ie>
2021-03-02 19:06:30 +0000

    Added the MAJ function and edited other parts in main method to output both CH and MAJ

--- funcs.c
+++ funcs.c
@@ -3,12 +3,10 @@
 
 #define WORD uint32_t
 #define PF PRIX32
-#define CH(x,y,z) (x&y)^(~x&z)
 
-WORD Ch(WORD x, WORD y, WORD z)
-{
-    return (x&y)^(~x&z);
-}
+// Page 10 of the secure hash standard
+#define CH(x,y,z) (x&y)^(~x&z)
+#define MAJ(x,y,z) (x&y)^( x&z)^(y&z)
 
 
 int main(int argc, char *argv[])
@@ -17,9 +15,13 @@ int main(int argc, char *argv[])
     WORD y = 0x0A0A0A0A;
     WORD z = 0xB0B0B0B0;
 
-    WORD ans = CH(x, y, z);
+    WORD ans;
 
+    ans = CH(x,y,z);
     printf("Ch(%08" PF ",%08" PF ",%08" PF ")=%08" PF "\n", x, y, z, ans);
+
+    ans = MAJ(x,y,z);
+    printf("Maj(%08" PF ",%08" PF ",%08" PF ")=%08" PF "\n", x, y, z, ans);
 
     return 0;
 }
