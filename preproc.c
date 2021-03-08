ac26fb8cd59214fa8c66eeda272a1499908885e1
JackH97 <g00359747@gmit.ie>
2021-03-08 17:23:25 +0000

    Continued on with preproc.c file creating more in main method with file and nobytes

--- preproc.c
+++ preproc.c
@@ -1,11 +1,22 @@
 #include <stdio.h>
 #include <inttypes.h>
 
+#define BYTE uint8_t
 
 int main(int argc, char *argv[]) {
+    BYTE b;
+
     FILE *f;
-    
-    
+    f = fopen(argv[1], "r");
+
+    size_t nobytes;
+
+    nobytes = fread(&b, 1, 1, f);
+    while (nobytes) {
+        nobytes = fread(&b, 1, 1, f);
+    }
+    fclose(f);
+
     
     return 0;
 }
