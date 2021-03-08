252eb3571d02a0c625370d26d7b84735aee107e6
JackH97 <g00359747@gmit.ie>
2021-03-08 17:27:21 +0000

    Added printf methods to last commit

--- preproc.c
+++ preproc.c
@@ -12,10 +12,13 @@ int main(int argc, char *argv[]) {
     size_t nobytes;
 
     nobytes = fread(&b, 1, 1, f);
+    printf("%c", b);
     while (nobytes) {
         nobytes = fread(&b, 1, 1, f);
+        printf("%c", b);
     }
     fclose(f);
+    printf("\n")
 
     
     return 0;
